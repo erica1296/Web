38f2e78dc956fcbe2498b65311d196d0bb9fecc7
erica1296 <tarqui.tarqui.erica@itdonbosco.org>
2025-04-30 13:28:23 -0400

    actualizando el proyecto

--- app.py
+++ app.py
@@ -8,12 +8,20 @@ def home():
 
 @app.route('/about')
 def about():
-    return "<h1><strong>Esta es la página de About.</strong></h1><p>Esta es una página de ejemplo para mostrar cómo funciona Flask.</p>"
+    return render_template('about.html')
 
 @app.route('/tasks')
 def list_tasks():
-    tarea = "Lavar la ropa"
-    return render_template('tasks.html', t=tarea)
+    tareas = ["Lavar la ropa", "Limpiar la casa", "Hacer la compra", "Estudiar para el examen", "Hacer ejercicio", "Leer un libro"]
+    return render_template('tasks.html', tareas=tareas)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5001)
