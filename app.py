0a9352259d8af5646a84510d5b760a8cbac694cd
erica1296 <tarqui.tarqui.erica@itdonbosco.org>
2025-05-08 13:32:31 -0400

    clase 8

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, session
+from config import Config
+from models import db, Usuario, Tarea
 
 app = Flask(__name__)
+#Configuración de la aplicación Flask (donde se indica la base de datos a usar)
+app.config.from_object(Config)
+
+# Inicializar la base de datos
+db.init_app(app)
+# Crear la base de datos si no existe
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        correo = request.form['correo']
+        contrasena = request.form['contrasena']
+        
+        usuario = Usuario.query.filter_by(correo=correo).first()
+        if usuario and usuario.verificar_contrasena(contrasena):
+            session["usuario_id"] = usuario.id
+            session["usuario_nombre"] = usuario.nombre
+            return redirect(url_for('list_tasks'))
+        else:
+            return "Correo o contraseña incorrectos."
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        contrasena = request.form['contrasena']
+        if Usuario.query.filter_by(correo=correo).first():
+            return "El correo ya está registrado."
+        else:
+            nuevo_usuario = Usuario(nombre=nombre, correo=correo)
+            nuevo_usuario.colocar_contrasena(contrasena)
+            db.session.add(nuevo_usuario)
+            db.session.commit()
+            return redirect(url_for('login'))
+    return render_template('signup.html')
 
 @app.route('/about')
 def about():
@@ -12,24 +58,19 @@ def about():
 
 @app.route('/tasks')
 def list_tasks():
-    tareas = ["Lavar la ropa", "Limpiar la casa", "Hacer la compra", "Estudiar para el examen", "Hacer ejercicio", "Leer un libro"]
+    tareas = Tarea.query.filter_by(usuario_id=session["ususario_id"]).all()
     return render_template('tasks.html', tareas=tareas)
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
+@app.route('/task')
+def view_task():
+    return render_template('task.html')
 
 @app.route('/task/create')
 def create_task():
     return render_template('create_task.html')
+#Crear una ruta y la vista correspondiente para renderizar un html llamado "create_task.html"
 
-@app.route('/task')
-def task():
-    return render_template('task.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5001)
--- models.py
+++ models.py
@@ -1,27 +1,33 @@
-from Flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
-db = SQLAlchemy
+db = SQLAlchemy()
 
-class usuario(db.Model):
-    __tablename__= 'usuarios'
-
-    id = db.Column(db.integer, primary_key=True)
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    
+    id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(200), nullable=False)
     correo = db.Column(db.String(100), unique=True, nullable=False)
-    contraseña = db.Column(db.Sctring(256), nullable=False)
+    contrasena_hash = db.Column(db.String(256), nullable=False)
+    
+    tareas = db.relationship('Tarea', backref='usuario', lazy=True)
+    
+    def colocar_contrasena(self, contrasena):
+        self.contrasena_hash = generate_password_hash(contrasena)
+    
+    def verificar_contrasena(self, contrasena):
+        return check_password_hash(self.contrasena_hash, contrasena)
 
-
-
-class Tarea(db.model):
-    __tablename__ = 'tarea'
-
-    id = db.Column(db.integer, primary_key=True)
-    titulo = db.Column(db.Strimg(200), nullable=False)
+class Tarea(db.Model):
+    __tablename__ = 'tareas'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(200), nullable=False)
     descripcion = db.Column(db.Text, nullable=True)
-    fecha_creacion = db.Column(db.DateTime, default=datetime.uttenow)
+    fecha_creacion = db.Column(db.DateTime, default=datetime.utcnow)
     fecha_vencimiento = db.Column(db.DateTime, nullable=False)
     completada = db.Column(db.Boolean, default=False)
     prioridad = db.Column(db.String(50), nullable=False)
-    usuario_id = db.Column(db.Integre, db.Foreign_key('usuarios.id'), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
